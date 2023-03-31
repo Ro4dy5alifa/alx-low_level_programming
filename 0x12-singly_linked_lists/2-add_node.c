3f7e69cbba5febe2a61b824fb77718178fe5be7f
Ro4dy5alif <12122012rhfs3355618@gmail.com>
2023-03-31 14:11:24 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,43 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
- * @head: double pointer to the list_t list
- * @str: new string to add in the node
+ * add_node - Add new node to a linked list.
+ * @head: head of the linked list.
+ * @str: string to add.
  *
- * Return: the address of the new element, or NULL if it fails
+ *
+ * Return: pointer of the new element added to linked list.
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
- list_t *new;
- unsigned int len = 0;
+	list_t *new;
 
- while (str[len])
- len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+/**
+ * _strlen - Calculate lenght of a string.
+ * @s: string.
+ *
+ * Return: lenght of the string s.
+ */
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+int _strlen(const char *s)
+{
+	int idx = 0;
 
- return (*head);
+	for (; s[idx]; idx++)
+	;
+	return (idx);
 }
